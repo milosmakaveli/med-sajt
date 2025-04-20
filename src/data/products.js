0954bbaf360788c19b14056fc372a59d0debf367
milosmakaveli <milos07.marjanovic@gmail.com>
2025-04-20 05:19:59 +0200

    cleaning3

--- src/data/products.js
+++ src/data/products.js
@@ -1,40 +1,45 @@
+import Bagremov from '../assets/Bagremov.jpg'
+import Medusacu from '../assets/medusacu.jpg'
+import Propolis from '../assets/propolis.jpg'
+import Zara from '../assets/zara.jpg'
+import Medsapolenom from '../assets/Medsapolenom.jpg'
+
 const products = [
     {
       id: 1,
       name: "Bagremov med",
       price: 1200,
-      image: process.env.PUBLIC_URL + "../assets/Bagremov.jpg",
+      image: Bagremov,
       description: "Med od bagrema poseduje blage antibakterijske i protivzapaljenske osobine..."
     },
     {
       id: 2,
       name: "Med u saću",
       price: 2000,
-      image: process.env.PUBLIC_URL + "../assets/medusacu.jpg",
+      image: Medusacu,
       description: "Med u saću ima antibakterijsko, antivirusno i antigljivično dejstvo..."
     },
     {
       id: 3,
       name: "Propolis kapi",
       price: 250,
-      image: process.env.PUBLIC_URL + "../assets/propolis.jpg",
+      image: Propolis,
       description: "Savremena istraživanja naglašavaju dobra svojstva propolisa kao leka..."
     },
     {
       id: 4,
       name: "Med sa žaraom",
       price: 1500,
-      image: process.env.PUBLIC_URL + "../assets/zara.jpg",
+      image: Zara,
       description: "Med sa semenom koprive je prirodni eliksir bogat gvožđem..."
     },
     {
       id: 5,
       name: "Med sa polenom",
       price: 1700,
-      image: process.env.PUBLIC_URL + "../assets/Medsapolenom.jpg",
+      image: Medsapolenom,
       description: "Ova izuzetna mešavina je poznata kao eliksir dugovečnosti..."
     }
   ];
   
   export default products;
-  
