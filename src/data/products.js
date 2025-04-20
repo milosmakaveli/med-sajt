4712ccb958f7d300b77d5147ca1772ca94e7e8fa
milosmakaveli <milos07.marjanovic@gmail.com>
2025-04-20 04:54:21 +0200

    fixed some imports

--- src/data/products.js
+++ src/data/products.js
@@ -1,37 +1,44 @@
+import Bagremov from '../assets/Bagremov.jpg'
+import medusacu from '../assets/medusacu.jpg'
+import propolis from '../assets/propolis.jpg'
+import zara from '../assets/zara.jpg'
+import Medsapolenom from '../assets/Medsapolenom.jpg'
+
+
 const products = [
     {
         id: 1,
         name: "Bagremov med",
         price: 1200,
-        image: "src/assets/Bagremov.jpg",
+        image: Bagremov,
         description: "Med od bagrema poseduje blage antibakterijske i protivzapaljenske osobine. Odličan je antiseptik, stoga se može koristiti u zaceljivanju sitnih povreda i posekotina. Uz umerenu fizičku aktivnost konzumiranje meda od bagrema topi kilograme."
     },
     {
         id: 2,
         name: "Med u saću",
         price: 2000,
-        image: "src/assets/medusacu.jpg",
+        image: medusacu,
         description: "Med u saću ima antibakterijsko, antivirusno i antigljivično dejstvo. Žvakanje voštane mase saća zajedno sa medom povoljno deluje na disajne puteve i sinuse."
     },
     {
         id: 3,
         name: "Propolis kapi",
         price: 250,
-        image: "src/assets/propolis.jpg",
+        image: propolis,
         description: "Savremena istraživanja naglašavaju dobra svojstva propolisa kao leka za zarastanje rana, koristi se kao sredstvo za oralnu higijenu, protiv upala, za lečenje herpesa na usnama."
     },
     {
         id: 4,
         name: "Med sa žaraom",
         price: 1500,
-        image: "src/assets/zara.jpg",
+        image: zara,
         description: "Med sa semenom koprive je prirodni eliksir bogat gvožđem, vitaminima i antioksidantima koji jačaju imunitet, poboljšavaju krvnu sliku i podstiču cirkulaciju."
     },
     {
         id: 5,
         name: "Med sa polenom",
         price: 1700,
-        image: "src/assets/Medsapolenom.jpg",
+        image: Medsapolenom,
         description: "Ova izuzetna mešavina je poznata kao eliksir dugovečnosti, a koristiti se kao imunostimulans za jačanje imuniteta."
     }
 ];
