9b7549b2c5cc699fd9d250d2e7da76e6e29994e3
milosmakaveli <milos07.marjanovic@gmail.com>
2025-04-20 05:01:35 +0200

    fixed some more inputs

--- src/data/products.js
+++ src/data/products.js
@@ -1,46 +1,40 @@
-import Bagremov from '../assets/Bagremov.jpg'
-import medusacu from '../assets/medusacu.jpg'
-import propolis from '../assets/propolis.jpg'
-import zara from '../assets/zara.jpg'
-import Medsapolenom from '../assets/Medsapolenom.jpg'
-
-
 const products = [
     {
-        id: 1,
-        name: "Bagremov med",
-        price: 1200,
-        image: Bagremov,
-        description: "Med od bagrema poseduje blage antibakterijske i protivzapaljenske osobine. Odličan je antiseptik, stoga se može koristiti u zaceljivanju sitnih povreda i posekotina. Uz umerenu fizičku aktivnost konzumiranje meda od bagrema topi kilograme."
+      id: 1,
+      name: "Bagremov med",
+      price: 1200,
+      image: process.env.PUBLIC_URL + "/assets/Bagremov.jpg",
+      description: "Med od bagrema poseduje blage antibakterijske i protivzapaljenske osobine..."
     },
     {
-        id: 2,
-        name: "Med u saću",
-        price: 2000,
-        image: medusacu,
-        description: "Med u saću ima antibakterijsko, antivirusno i antigljivično dejstvo. Žvakanje voštane mase saća zajedno sa medom povoljno deluje na disajne puteve i sinuse."
+      id: 2,
+      name: "Med u saću",
+      price: 2000,
+      image: process.env.PUBLIC_URL + "/assets/medusacu.jpg",
+      description: "Med u saću ima antibakterijsko, antivirusno i antigljivično dejstvo..."
     },
     {
-        id: 3,
-        name: "Propolis kapi",
-        price: 250,
-        image: propolis,
-        description: "Savremena istraživanja naglašavaju dobra svojstva propolisa kao leka za zarastanje rana, koristi se kao sredstvo za oralnu higijenu, protiv upala, za lečenje herpesa na usnama."
+      id: 3,
+      name: "Propolis kapi",
+      price: 250,
+      image: process.env.PUBLIC_URL + "/assets/propolis.jpg",
+      description: "Savremena istraživanja naglašavaju dobra svojstva propolisa kao leka..."
     },
     {
-        id: 4,
-        name: "Med sa žaraom",
-        price: 1500,
-        image: zara,
-        description: "Med sa semenom koprive je prirodni eliksir bogat gvožđem, vitaminima i antioksidantima koji jačaju imunitet, poboljšavaju krvnu sliku i podstiču cirkulaciju."
+      id: 4,
+      name: "Med sa žaraom",
+      price: 1500,
+      image: process.env.PUBLIC_URL + "/assets/zara.jpg",
+      description: "Med sa semenom koprive je prirodni eliksir bogat gvožđem..."
     },
     {
-        id: 5,
-        name: "Med sa polenom",
-        price: 1700,
-        image: Medsapolenom,
-        description: "Ova izuzetna mešavina je poznata kao eliksir dugovečnosti, a koristiti se kao imunostimulans za jačanje imuniteta."
+      id: 5,
+      name: "Med sa polenom",
+      price: 1700,
+      image: process.env.PUBLIC_URL + "/assets/Medsapolenom.jpg",
+      description: "Ova izuzetna mešavina je poznata kao eliksir dugovečnosti..."
     }
-];
-
-export default products;
+  ];
+  
+  export default products;
+  
