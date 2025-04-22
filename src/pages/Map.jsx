6106bd69f8b291e60aeb2fc2dd441ca1fd2ca5dc
milosmakaveli <milos07.marjanovic@gmail.com>
2025-04-22 04:29:02 +0200

    fixed broken map pointer

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,7 +1,18 @@
 import React, { useEffect, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css'; 
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import './Map.css'; 
+
+
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const Map = () => {
   const mapContainer = useRef(null);
